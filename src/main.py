8ea213053e5571fd5782ec151a025a2a585c7223
Fradet <axel.fradet@epitech.eu>
2024-11-04 14:45:57 +0100

    Feat(Struct): Add the Makefile and main.py

---
+++ src/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+##
+## EPITECH PROJECT, 2024
+## Gomoku
+## File description:
+## main
+##
+
+def main():
+    while True:
+        command = str(input(''))
+        print("name=\"SomeBrain\", version=\"1.0\", author=\"FP\", country=\"FR\"")
+        print("OK")
+
+if __name__ == "__main__":
+    main()
